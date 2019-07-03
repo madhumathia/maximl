97579e30e0fe5758a4a12e19aad762c48dc2790e
madhumathia <34538824+madhumathia@users.noreply.github.com>
2019-07-03 11:56:34 +0530

    Main.java

---
+++ Main.java
@@ -0,0 +1,40 @@
+ class Main
+{
+    public static void main(String args[])
+    {
+    Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        LongestSubstring(str);
+
+    }
+public static void LongestSubstring(String s)
+{
+	boolean[] flag = new boolean[256];
+ 	int res = 0;
+	int start = 0;
+	char[] ch = s.toCharArray();
+ 
+	for (int i = 0; i < ch.length; i++) {
+		char current = ch[i];
+		if (flag[current]) {
+			res = Math.max(result, i - start);
+			for (int k = start; k < i; k++)
+			{
+				if (ch[k] == current)
+				{
+					start = k + 1; 
+					break;
+				}
+				flag[ch[k]] = false;
+			}
+		} else
+		{
+			flag[current] = true;
+		}
+	}
+ 
+	result = Math.max(ch.length - start, res);
+ 
+	System.out.println(res);
+}
+}
